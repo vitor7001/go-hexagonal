6e5b2ba82b7bdc5888904c085840943c97b4437a
vitorgomes7001 <vitorgomes7001@gmail.com>
2021-03-29 17:26:09 +0000

    feat: teste isvalid

--- application/product.go
+++ application/product.go
@@ -1,6 +1,13 @@
 package application
 
-import "errors"
+import (
+	"errors"
+	"github.com/asaskevich/govalidator"
+)
+
+func init(){
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type ProductInterface interface {
 	IsValid() (bool, error)
@@ -18,13 +25,31 @@ const (
 )
 
 type Product struct {
-	ID     string
-	Name   string
-	Price  float64
-	Status string
+	ID     string 	`valid:"uuidv4"`
+	Name   string	`valid:"required"`
+	Price  float64	`valid:"float,optional"`
+	Status string	`valid:"required"`
 }
 
 func (p *Product) IsValid() (bool, error) {
+
+	if p.Status == ""{
+		p.Status = DISABLED
+	}
+
+	if p.Status != ENABLED && p.Status != DISABLED{
+		return false, errors.New("the status must be enabled or disabled")
+	}
+
+	if p.Price <0{
+		return false, errors.New("the price must be greater or equal zero")
+	}
+
+	_, err := govalidator.ValidateStruct(p)
+	if err != nil {
+		return false, err
+	}
+
 	return true, nil
 }
 
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/vitor7001/go-hexagonal
 go 1.16
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
