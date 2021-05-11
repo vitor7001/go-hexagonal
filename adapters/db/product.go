bd4f63b0e07faac7986baf001807b6462794da94
vitorgomes7001 <vitorgomes7001@gmail.com>
2021-05-11 22:30:51 +0000

    feat: testando get Product

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -1,16 +1,17 @@
 package db
 
-import ( 
+import (
 	"database/sql"
+
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/vitor7001/go-hexagonal/application"
 )
 
 type ProductDb struct {
 	db *sql.DB
 }
 
-
-func ( p *ProductDb) Get(id string) (application.ProductInterface, err) {
+func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 	var product application.Product
 
 	stmt, err := p.db.Prepare("select id, name, price, status from products where id = ?")
@@ -21,9 +22,13 @@ func ( p *ProductDb) Get(id string) (application.ProductInterface, err) {
 
 	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
 
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
 	return &product, nil
+}
+
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db: db}
 }
