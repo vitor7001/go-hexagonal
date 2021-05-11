47a0ae8f5a1988a38a357d612f80c53af293d192
vitorgomes7001 <vitorgomes7001@gmail.com>
2021-05-11 22:44:23 +0000

    feat: save product

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -11,6 +11,10 @@ type ProductDb struct {
 	db *sql.DB
 }
 
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db: db}
+}
+
 func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 	var product application.Product
 
@@ -29,6 +33,61 @@ func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 	return &product, nil
 }
 
-func NewProductDb(db *sql.DB) *ProductDb {
-	return &ProductDb{db: db}
+func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
+
+	var rows int
+
+	p.db.QueryRow("Select id from products where id = ?", product.GetId()).Scan((&rows))
+
+	if rows == 0 {
+		_, err := p.create(product)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+ 		_, err := p.update((product)
+ 		if err != nil {
+			return nil, err
+		}
+	}
+	return product, nil
+}
+
+func (p *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
+
+	stmt, err := p.db.Prepare(`insert into products(id,name,price,status)
+	values(?,?,?,?)`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmt.Exec(
+		product.GetId(),
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) update(product application.ProductInterface) (application.ProductInterface, error) {
+	_, err := p.db.Exec("update products set name = ?, price = ?, status = ? where id = ?",
+		product.GetName(), product.GetPrice(), product.GetStatus(), product.GetId())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
 }
